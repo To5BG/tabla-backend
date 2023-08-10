968a33f9d4cecd137c3c5a34282c34014c65cfb7
AlpiGun5 <a.i.guncan02@gmail.com>
2023-08-11 00:35:11 +0300

    Fix the cookie parser signature

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.use(cookieParser(process.env.REFRESH_COOKIE_SECRET));
+  app.use(helmet());
   await app.listen(process.env.PORT || 3001);
 }
 
